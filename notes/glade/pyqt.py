38f0df49a10bea3d09d92602a6b1fdcf5cee9f58
engartst <engartst@gmail.com>
2023-05-31 10:44:34 -0400

    remove glade issues

--- notes/glade/pyqt.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from PySide6.QtWidgets import QApplication, QLabel
-app = QApplication([])
-label = QLabel('Hello World!')
-label.show()
-app.exec()
--- notes/glade/pysimple.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import PySimpleGUIQt as sg
-
-#sg.theme('DarkAmber')   # Add a touch of color
-# All the stuff inside your window.
-sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
